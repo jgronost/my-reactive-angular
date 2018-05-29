f65ed0d4e75ea42bc71362496b79b1f41933c935
Jaroslaw Gronostajski <jaroslaw,gronostajski@kwsoft.com>
2018-05-29 20:53:20 +0200

    note detail implemented

--- src/app/notes/note-detail/note-detail.component.ts
+++ src/app/notes/note-detail/note-detail.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from "@angular/core";
+import {Note} from "../note";
+import {ActivatedRoute, Params, Router} from "@angular/router";
+import {NotesService} from "../notes.service";
 
 @Component({
   selector: "app-note-detail",
@@ -6,10 +9,28 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./note-detail.component.css"]
 })
 export class NoteDetailComponent implements OnInit {
+  note: Note;
+  id: number;
 
-  constructor() { }
+  constructor(private currentRoute: ActivatedRoute,
+              private router: Router,
+              private notesService: NotesService) { }
 
   ngOnInit() {
+    this.currentRoute.params.subscribe(
+      (params: Params) => {
+        this.id = +params["id"];   // plus to convert to number
+        this.note = this.notesService.getNote(this.id);
+      });
+  }
+
+  onEditNote() {
+    this.router.navigate(["edit"], {relativeTo: this.currentRoute});
+  }
+
+  onDeleteNote() {
+    this.notesService.deleteNote(this.id);
+    this.router.navigate(["/notes"]);
   }
 
 }
